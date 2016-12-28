2294b67633c4d19bd7ec157287db5a1d631298bc
Sergiy Perevyazko <sg.sergiyp@gmail.com>
2016-12-28 18:11:02 +0200

    Created SecurityConfig class, and implemented successfully auth handler.

---
+++ src/main/java/ua/softgroup/medreview/persistent/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package ua.softgroup.medreview.persistent.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import ua.softgroup.medreview.web.security.AuthSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private AuthSuccessHandler authSuccessHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/custom/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .successHandler(authSuccessHandler)
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .invalidateHttpSession(true);
+//                .and().userDetailsService()
+
+    }
+}
