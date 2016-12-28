4ebf8a627e0b3efa67130cbb5664bc6cfe20947a
Sergiy Perevyazko <sg.sergiyp@gmail.com>
2016-12-28 16:57:16 +0200

    Implemented UserDetails interface. Added user roles.

---
+++ src/main/java/ua/softgroup/medreview/persistent/entity/UserRole.java
@@ -0,0 +1,45 @@
+package ua.softgroup.medreview.persistent.entity;
+
+import javax.persistence.*;
+/**
+ * Created by Sergiy Perevyazko on 28.12.2016.
+ */
+@Entity
+public class UserRole extends AbstractEntity<Long> {
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne
+    private User user;
+
+    public UserRole() {
+    }
+
+    public UserRole(Role role, User user) {
+        this.role = role;
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return role.toString();
+    }
+}
