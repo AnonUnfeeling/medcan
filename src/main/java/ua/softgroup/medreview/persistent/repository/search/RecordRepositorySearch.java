1e4194161bd80adfcbb2a734f5309bd7b137e316
Oleksandr Tyshkovets <sg.olexander@gmail.com>
2016-12-30 15:43:22 +0200

    Enhance record search with option to search by creation date

--- src/main/java/ua/softgroup/medreview/persistent/repository/search/RecordRepositorySearch.java
+++ src/main/java/ua/softgroup/medreview/persistent/repository/search/RecordRepositorySearch.java
@@ -2,6 +2,7 @@ package ua.softgroup.medreview.persistent.repository.search;
 
 import ua.softgroup.medreview.persistent.entity.Record;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -13,8 +14,10 @@ public interface RecordRepositorySearch {
      * Searches records by title's keywords
      *
      * @param keywords the keywords to search, separate by blanks
+     * @param from optional date range (start)
+     * @param to optional date range (end)
      * @return a list of matching records
      */
-    List<Record> searchByTitle(String keywords);
+    List<Record> searchByTitle(String keywords, LocalDate from, LocalDate to);
 
 }
--- src/main/java/ua/softgroup/medreview/persistent/repository/search/impl/RecordRepositoryImpl.java
+++ src/main/java/ua/softgroup/medreview/persistent/repository/search/impl/RecordRepositoryImpl.java
@@ -1,15 +1,17 @@
 package ua.softgroup.medreview.persistent.repository.search.impl;
 
-import org.apache.lucene.search.Query;
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.BooleanJunction;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.springframework.transaction.annotation.Transactional;
 import ua.softgroup.medreview.persistent.entity.Record;
 import ua.softgroup.medreview.persistent.repository.search.RecordRepositorySearch;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Oleksandr Tyshkovets <sg.olexander@gmail.com>
@@ -17,6 +19,7 @@ import java.util.List;
 public class RecordRepositoryImpl implements RecordRepositorySearch {
 
     private static final String RECORD_TITLE_FIELD = "title";
+    private static final String RECORD_CREATION_DATE_FIELD = "creationDate";
 
     private final EntityManager entityManager;
 
@@ -26,7 +29,7 @@ public class RecordRepositoryImpl implements RecordRepositorySearch {
 
     @Override
     @Transactional(readOnly = true)
-    public List<Record> searchByTitle(final String keywords) {
+    public List<Record> searchByTitle(final String keywords, final LocalDate from, final LocalDate to) {
         FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
 
         QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory()
@@ -34,14 +37,36 @@ public class RecordRepositoryImpl implements RecordRepositorySearch {
                 .forEntity(Record.class)
                 .get();
 
-        Query query = queryBuilder
+        BooleanJunction<BooleanJunction> junction = createJunction(keywords, queryBuilder);
+        addDateRangeToJunction(from, to, queryBuilder, junction);
+
+        //noinspection unchecked
+        return fullTextEntityManager
+                .createFullTextQuery(junction.createQuery(), Record.class)
+                .getResultList();
+    }
+
+    private BooleanJunction<BooleanJunction> createJunction(String keywords, QueryBuilder queryBuilder) {
+        BooleanJunction<BooleanJunction> junction = queryBuilder.bool();
+        junction.must(queryBuilder
                 .keyword()
                 .onFields(RECORD_TITLE_FIELD)
                 .matching(keywords)
-                .createQuery();
+                .createQuery()
+        );
+        return junction;
+    }
 
-        //noinspection unchecked
-        return fullTextEntityManager.createFullTextQuery(query, Record.class).getResultList();
+    private void addDateRangeToJunction(LocalDate from, LocalDate to, QueryBuilder queryBuilder,
+                                        BooleanJunction<BooleanJunction> junction) {
+        Optional.ofNullable(from)
+                .map(LocalDate::atStartOfDay)
+                .map(date -> queryBuilder.range().onField(RECORD_CREATION_DATE_FIELD).above(date).createQuery())
+                .ifPresent(junction::must);
+        Optional.ofNullable(to)
+                .map(date -> date.plusDays(1).atStartOfDay())
+                .map(date -> queryBuilder.range().onField(RECORD_CREATION_DATE_FIELD).below(date).excludeLimit().createQuery())
+                .ifPresent(junction::must);
     }
 
 }
