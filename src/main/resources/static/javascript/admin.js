83e45897be3d0b324aebba217645312ee61a2c72
Vladimir Pihol <sg.volodymyr@gmail.com>
2016-12-30 11:52:38 +0200

    Add delete function for companies, add sign out button, change some styles

--- src/main/resources/static/javascript/admin.js
+++ src/main/resources/static/javascript/admin.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
+    var company_control = $('.company-control');
 
+    $('[data-toggle=confirmation]').confirmation({
+        rootSelector: '[data-toggle=confirmation]'
+    });
+
+    company_control.click(function () {
+        var control = $(this);
+        control.confirmation('show');
+        $('#table-body').on('confirmed.bs.confirmation', deleteCompany(control));
+    });
 });
+
+function deleteCompany(control) {
+    $(control).parent().parent().remove();
+    $.ajax({
+        method: "POST",
+        url: "/removeCompany",
+        data: { companyName: control.attr('id')},
+        dataType: "json"
+    }).fail(function (data) {
+        console.log(data);
+    });
+}
 
 function createCompany() {
     var success_msg = $('#success');
@@ -12,7 +34,7 @@ function createCompany() {
         dataType: "json"
         }).fail(function (data) {
         console.log(data);
-        if (data=='CREATED'){
+        if (data.responseText=='CREATED'){
             success_msg.hide();
             error_msg.hide();
             success_msg.show();
