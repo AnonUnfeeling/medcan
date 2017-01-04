bfd0a3657d233aafacf316a3a1688d4048563c1d
Sergiy Perevyazko <sg.sergiyp@gmail.com>
2017-01-04 19:23:43 +0200

    Create Subject and SubSubject entities. Create SubjectRepository, implement Subject and SubSubject services.

---
+++ src/main/java/ua/softgroup/medreview/persistent/entity/SubSubject.java
@@ -0,0 +1,47 @@
+package ua.softgroup.medreview.persistent.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author Sergiy Perevyazko <sg.sergiyp@gmail.com>
+ */
+@Entity
+public class SubSubject extends AbstractEntity<Long> {
+    private static final long serialVersionUID = 98000298364542854L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    private Subject subject;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+}
---
+++ src/main/java/ua/softgroup/medreview/persistent/entity/Subject.java
@@ -0,0 +1,54 @@
+package ua.softgroup.medreview.persistent.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sergiy Perevyazko <sg.sergiyp@gmail.com>
+ */
+@Entity
+public class Subject extends AbstractEntity<Long> {
+    private static final long serialVersionUID = 7319584613784286792L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SubSubject> subSubjects = new ArrayList<>();
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SubSubject> getSubSubjects() {
+        return subSubjects;
+    }
+
+    public void setSubSubjects(List<SubSubject> subSubjects) {
+        this.subSubjects.clear();
+        if (subSubjects != null) {
+            subSubjects.forEach(subSubject -> subSubject.setSubject(this));
+            this.subSubjects.addAll(subSubjects);
+        }
+        this.subSubjects = subSubjects;
+    }
+}
