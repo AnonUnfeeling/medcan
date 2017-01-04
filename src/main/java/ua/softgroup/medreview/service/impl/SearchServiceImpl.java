61b285cf165db8ad46b65591a2dfb847f3f1606b
Sergiy Perevyazko <sg.sergiyp@gmail.com>
2017-01-04 18:39:51 +0200

    Add some changes to SearchService and AuthenticationService.

--- src/main/java/ua/softgroup/medreview/service/impl/SearchServiceImpl.java
+++ src/main/java/ua/softgroup/medreview/service/impl/SearchServiceImpl.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.softgroup.medreview.persistent.entity.Note;
 import ua.softgroup.medreview.persistent.entity.Record;
+import ua.softgroup.medreview.persistent.entity.Role;
 import ua.softgroup.medreview.persistent.repository.RecordRepository;
 import ua.softgroup.medreview.persistent.repository.NoteRepository;
+import ua.softgroup.medreview.service.AuthenticationService;
 import ua.softgroup.medreview.service.SearchService;
 
 import java.time.LocalDate;
@@ -21,22 +22,27 @@ public class SearchServiceImpl implements SearchService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final NoteRepository noteRepository;
     private final RecordRepository recordRepository;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public SearchServiceImpl(NoteRepository noteRepository, RecordRepository recordRepository) {
+    public SearchServiceImpl(NoteRepository noteRepository, RecordRepository recordRepository, AuthenticationService authenticationService) {
         this.noteRepository = noteRepository;
         this.recordRepository = recordRepository;
+        this.authenticationService = authenticationService;
     }
 
     @Override
-    public List<Note> searchByKeywords(String keywords, LocalDate from, LocalDate to) {
-        logger.debug("searchByKeywords. keywords: {}, from: {}, to: {}", keywords, from, to);
-        return noteRepository.searchByKeywords(keywords, from, to);
-    }
-
-    @Override
-    public List<Record> searchByTitle(String keywords, LocalDate from, LocalDate to) {
-        logger.debug("searchByTitle. keywords:  {}, from: {}, to: {}");
-        return recordRepository.searchByTitle(keywords, from, to);
+    public List<Record> searchByTitle(String text, LocalDate from, LocalDate to) {
+        logger.debug("searchByTitle. text:  {}, from: {}, to: {}", text, from, to);
+        List<String> userRoles = authenticationService.getUserRoles();
+        if (userRoles.contains(Role.ADMIN.name())) {
+            return recordRepository.searchByTitle(text, from, to);
+        } else if (userRoles.contains(Role.COMPANY.name())) {
+            //TODO: must be implemented
+            return null;
+        } else if (userRoles.contains(Role.USER.name())) {
+            return recordRepository.searchByTitleAndAuthor(authenticationService.getPrincipal().getUsername(), text, from, to);
+        }
+        return null;
     }
 }
