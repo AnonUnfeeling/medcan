9296cd1e85bfc47bc8b3e30ae537937d519bfce4
jdroidcoder <anonunfeeling1@gmail.com>
2017-01-05 17:15:01 +0200

    fix search for note

--- src/main/resources/static/javascript/searchNote.js
+++ src/main/resources/static/javascript/searchNote.js
@@ -47,8 +47,75 @@ $(document).ready(function () {
     });
 
 });
+/**
+ * Created by jdroidcoder on 03.01.2017.
+ */
+
+var token = $("meta[name='_csrf']").attr("content");
+
+$(document).ready(function () {
+    checkUser();
+    loadNotes();
+});
+
+function checkUser() {
+    $.ajax({
+        method: "GET",
+        url: "/records/getRecord",
+        data: {recordName: $('#titleNote').val()},
+        dataType: "json",
+        headers: {
+            'X-CSRF-TOKEN': token
+        }
+    }).done(function (data) {
+        if (data == false) {
+            $('#addNoteButton').hide();
+            $('#submitButton').hide();
+        }
+    });
+}
+
+function loadNotes() {
+    $.ajax({
+        method: "GET",
+        url: "/records/notes",
+        data: {title: $('#titleNote').val()},
+        dataType: "json",
+        headers: {
+            'X-CSRF-TOKEN': token
+        }
+    }).done(function (data) {
+        var arr = data;
+        var table = $('#table-body');
+        table.find('tr').remove();
+        $(arr).each(function () {
+            var note = $(this)[0];
+            table.append('<tr onclick="loadPreNote(event,this,' + $(this)[0].id + ');"><td>' + note.description.slice(0, 8) + '</td>' +
+                '<td>' + note.conclusion.slice(0, 8) + '</td>' +
+                '<td>' + note.keywords.slice(0, 8) + '</td>' +
+                '<td>' + note.subject.slice(0, 8) + '</td>' +
+                '<td>' + note.subSubject.slice(0, 8) + '</td>' +
+                '<td>' + note.treatment + '</td>' +
+                '<td>' + note.country.slice(0, 8) + '</td>' +
+                '<td>' + note.language.slice(0, 8) + '</td>' +
+                '<td>' + note.status.slice(0, 8) + '</td>' +
+                '<td>' + note.updateDate.dayOfMonth + ' ' + note.updateDate.month +
+                ' ' + note.updateDate.year +
+                '</td><td></td><td class="text-right"><span id="' +
+                note.id + '" data-singleton="true"' +
+                ' data-toggle="edit" class="glyphicon glyphicon glyphicon-pencil user-control" ' +
+                'aria-hidden="true"></span>' +
+                '<td class="text-right"><span id="' + note.id + '" ' +
+                'data-singleton="true" data-toggle="confirmation" ' +
+                'class="glyphicon glyphicon-remove-circle users-control" ' +
+                'aria-hidden="true"></span></td></tr>');
+        });
+        manageCompany();
+    });
+}
 
 var editId;
+var isEdit = false;
 
 function loadPreNote(event, note, id) {
     var e = event || window.event,
@@ -59,12 +126,16 @@ function loadPreNote(event, note, id) {
         elm = elm.parentNode;
     }
 
-    if (elm !== allTDs[10] && elm !== allTDs[11] && elm !== note) {
+    if (elm !== allTDs[11] && elm !== allTDs[12] && elm !== note) {
         editId = id;
+        isEdit = true;
         loadSubject();
         loadSubSubject();
         loadStatus();
+        loadTreatment();
         loadNote();
+        $('#submitButton').hide();
+        $('#titleFoNote').text("Details");
         var edit = $('#creteNote');
         edit.modal('show');
     }
@@ -90,6 +161,7 @@ function manageCompany() {
         var edit = $('#creteNote');
         var control = $(this);
         editId = control.attr('id');
+        $('#titleFoNote').text("Edit note");
         loadSubject();
         loadSubSubject();
         loadStatus();
@@ -101,6 +173,8 @@ function manageCompany() {
 
 $(document).on('hide.bs.modal', '#creteNote', function () {
     editId = null;
+    isEdit = false;
+    $('#titleFoNote').text("Creating new note");
     $('#submitButton').show();
     $('#descriptionNote').val("");
     $('#conclusionNote').val(null);
@@ -122,10 +196,6 @@ function loadNote() {
             'X-CSRF-TOKEN': token
         }
     }).done(function (data) {
-        $('#submitButton').hide();
-        var select = $('#statusNote').empty();
-        select.append('<option value="' + data.status + '">' +
-            '' + data.status + '</option>');
         $('#descriptionNote').val(data.description);
         $('#conclusionNote').val(data.conclusion);
         $('#keywordsNote').val(data.keywords);
@@ -134,6 +204,23 @@ function loadNote() {
         $('#countryNote').val(data.country);
         $('#languageNote').val(data.language);
         $('#treatmentNote').val(data.treatment);
+        var select = $('#statusNote').empty();
+        select.append('<option value="' + data.status + '">' +
+            '' + data.status + '</option>');
+        if (!isEdit) {
+            $.ajax({
+                method: "POST",
+                url: "/checkRole",
+                headers: {
+                    'X-CSRF-TOKEN': token
+                }
+            }).done(function (data) {
+                console.log(data);
+                if (data == "ADMIN") {
+                    loadStatus();
+                }
+            });
+        }
     });
 }
 
@@ -217,7 +304,7 @@ function update() {
 function loadStatus() {
     $.ajax({
         method: "GET",
-        url: "/getStatus",
+        url: "/notes/statuses",
         dataType: "json",
         headers: {
             'X-CSRF-TOKEN': token
